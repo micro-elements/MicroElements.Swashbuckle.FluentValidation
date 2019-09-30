089f6dd697171172e72a26765b4a007bcebc35f2
Alexey Petryashev <petriashev@gmail.com>
2019-09-30 23:39:24 +0300

    fix cake run

--- build.sh
+++ build.sh
@@ -75,7 +75,8 @@ dotnet restore $CAKE_PROPS_PATH --packages $TOOLS_DIR --source "$NUGET_URL"
 dotnet tool restore
 
 # Start Cake
+exec dotnet cake --help
 echo "Running build script..."
-CMD="dotnet cake \"$SCRIPT\" ${CAKE_ARGUMENTS[@]}"
+CMD="dotnet cake $SCRIPT ${CAKE_ARGUMENTS[@]}"
 echo $CMD
 exec $CMD
