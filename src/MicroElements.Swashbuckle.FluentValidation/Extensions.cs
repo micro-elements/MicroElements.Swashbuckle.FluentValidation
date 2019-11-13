1bf9ce5c1b41e95149dc598843e72fba1b5384f2
Roy van der Tuuk <rvdtuuk@routit.nl>
2019-11-13 11:07:59 +0100

    Fixes for invalid documentation on validation rules containing a condition.

--- src/MicroElements.Swashbuckle.FluentValidation/Extensions.cs
+++ src/MicroElements.Swashbuckle.FluentValidation/Extensions.cs
@@ -86,5 +86,15 @@ namespace MicroElements.Swashbuckle.FluentValidation
                 .Where(propertyRule => propertyRule.Condition == null && propertyRule.AsyncCondition == null && propertyRule.PropertyName?.Equals(name, StringComparison.InvariantCultureIgnoreCase) == true)
                 .SelectMany(propertyRule => propertyRule.Validators);
         }
+
+        /// <summary>
+        /// Returns a <see cref="bool"/> indicating if the <paramref name="propertyValidator"/> is conditional.
+        /// </summary>
+        /// <param name="propertyValidator"></param>
+        /// <returns></returns>
+        internal static bool HasNoCondition(this IPropertyValidator propertyValidator)
+        {
+            return propertyValidator?.Options?.Condition == null && propertyValidator?.Options?.AsyncCondition == null;
+        }
     }
 }
