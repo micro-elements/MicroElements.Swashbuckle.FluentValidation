1c31c484674553a9f90db73979571181be55ff76
Alexey Petryashev <petriashev@gmail.com>
2021-01-08 17:02:16 +0300

    Issue54 repro added

---
+++ samples/SampleWebApi/Controllers/Issue54.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class Issue54 : Controller
+    {
+        [HttpGet("[action]")]
+        public IActionResult Get(GetRequest query) => Ok();
+
+        public class GetRequest
+        {
+            [FromQuery]
+            public string Id { get; set; }
+        }
+
+        public class Validator : AbstractValidator<GetRequest>
+        {
+            public Validator()
+            {
+                RuleFor(c => c.Id)
+                    .MinimumLength(6);
+            }
+        }
+    }
+}
