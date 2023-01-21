99501e153970285ecf0ba0ac7571da8e8258112c
Alexey Petryashev <petriashev@gmail.com>
2023-01-21 22:03:02 +0300

    IServicesContext moved to separate class

---
+++ src/MicroElements.OpenApi.FluentValidation/FluentValidation/IServicesContext.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) MicroElements. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace MicroElements.OpenApi.FluentValidation
+{
+    /// <summary>
+    /// Services that can be injected with DI.
+    /// </summary>
+    public interface IServicesContext
+    {
+        /// <summary>
+        /// Gets optional <see cref="INameResolver"/>.
+        /// </summary>
+        INameResolver? NameResolver { get; }
+    }
+
+    /// <summary>
+    /// Services that can be injected with DI.
+    /// </summary>
+    public class ServicesContext : IServicesContext
+    {
+        /// <inheritdoc />
+        public INameResolver? NameResolver { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServicesContext"/> class.
+        /// </summary>
+        /// <param name="nameResolver">Optional name resolver.</param>
+        public ServicesContext(INameResolver? nameResolver = null)
+        {
+            NameResolver = nameResolver;
+        }
+    }
+}
