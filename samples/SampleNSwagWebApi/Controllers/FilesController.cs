03c187e5eb4f77feba10ef6cfc76870b94d8a0b2
Rami <icnocop@users.noreply.github.com>
2023-04-24 23:49:34 -0700

    IFormFile tests

---
+++ samples/SampleNSwagWebApi/Controllers/FilesController.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleNSwagWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        [HttpPost("[action]")]
+        public IActionResult UploadFile([FromForm] UploadFileRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+    }
+
+    #region Model
+
+    public class UploadFileRequest
+    {
+        [FromForm(Name = "File")]
+        public IFormFile File { get; set; }
+    }
+
+    #endregion
+
+    #region Validation
+
+    public class UploadFileRequestValidator : AbstractValidator<UploadFileRequest>
+    {
+        public UploadFileRequestValidator()
+        {
+            RuleFor(x => x.File)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty();
+        }
+    }
+
+    #endregion
+}
---
+++ test/MicroElements.Swashbuckle.FluentValidation.Tests/IFormFileTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Xunit;
+
+namespace MicroElements.Swashbuckle.FluentValidation.Tests
+{
+    public class IFormFileTests : UnitTestBase
+    {
+        public class UploadFileRequest
+        {
+            [FromForm(Name = "File")]
+            public IFormFile File { get; set; }
+        }
+
+        public class UploadFileRequestValidator : AbstractValidator<UploadFileRequest>
+        {
+            public UploadFileRequestValidator()
+            {
+                RuleFor(c => c.File)
+                    .Cascade(CascadeMode.Stop)
+                    .NotEmpty();
+            }
+        }
+
+        [Fact]
+        public void all_rules_should_be_applied()
+        {
+            var schemaRepository = new SchemaRepository();
+            var referenceSchema = SchemaGenerator(new UploadFileRequestValidator()).GenerateSchema(typeof(UploadFileRequest), schemaRepository);
+
+            var schema = schemaRepository.Schemas[referenceSchema.Reference.Id];
+            var fileProperty = schema.Properties[nameof(UploadFileRequest.File)];
+            fileProperty.Type.Should().Be("string");
+            fileProperty.Format.Should().Be("binary");
+            fileProperty.Nullable.Should().Be(false);
+        }
+    }
+}
