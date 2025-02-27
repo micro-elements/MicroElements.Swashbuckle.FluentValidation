fff82600847b490a3b2b55f9f9034215fdbd803d
kkytaiev <Kyrylo.Kytaiev@rwsworldwide.com>
2025-02-27 14:14:07 +0200

    Fix duplicate regular expression patterns in a generated schema Add unit test

--- src/MicroElements.OpenApi.FluentValidation/FluentValidation/RuleHistoryCache.cs
+++ src/MicroElements.OpenApi.FluentValidation/FluentValidation/RuleHistoryCache.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) MicroElements. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -16,7 +17,30 @@ namespace MicroElements.OpenApi.FluentValidation
             string SchemaTypeName,
             string SchemaPropertyName,
             IPropertyValidator PropertyValidator,
-            string RuleName);
+            string RuleName)
+        {
+            public virtual bool Equals(RuleCacheItem? other)
+            {
+                return !(other is null) &&
+                    SchemaTypeName == other.SchemaTypeName &&
+                    SchemaPropertyName == other.SchemaPropertyName &&
+                    PropertyValidatorComparer.Instance.Equals(PropertyValidator, other.PropertyValidator)
+                    && RuleName == other.RuleName;
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    PropertyValidator.Equals(PropertyValidator);
+                    var hashCode = SchemaTypeName.GetHashCode();
+                    hashCode = (hashCode * 397) ^ SchemaPropertyName.GetHashCode();
+                    hashCode = (hashCode * 397) ^ PropertyValidator.Name.GetHashCode();
+                    hashCode = (hashCode * 397) ^ RuleName.GetHashCode();
+                    return hashCode;
+                }
+            }
+        }
 
         internal static void AddRuleHistoryItem(this object schema, RuleCacheItem historyItem)
         {
@@ -28,6 +52,56 @@ namespace MicroElements.OpenApi.FluentValidation
         {
             List<RuleCacheItem> historyItems = SchemaRuleHistory.GetOrCreateValue(schema);
             return historyItems.FirstOrDefault(item => Equals(item, historyItem)) != null;
+        }
+
+        internal class PropertyValidatorComparer : IEqualityComparer<IPropertyValidator>
+        {
+            public static readonly PropertyValidatorComparer Instance = new PropertyValidatorComparer();
+
+            /// <inheritdoc />
+            public bool Equals(IPropertyValidator? x, IPropertyValidator? y)
+            {
+                if (ReferenceEquals(x, y))
+                    return true;
+
+                if (x == null || y == null)
+                    return false;
+
+                if (x.GetType() != y.GetType())
+                    return false;
+
+                // stateless validators
+                if (x is INotNullValidator || x is INotEmptyValidator || x is IEmailValidator)
+                    return true;
+
+                if (x is IRegularExpressionValidator expressionValidator1 &&
+                    y is IRegularExpressionValidator expressionValidator2)
+                    return Equals(expressionValidator1.Expression, expressionValidator2.Expression);
+
+                if (x is ILengthValidator lengthValidator1 &&
+                    y is ILengthValidator lengthValidator2)
+                    return lengthValidator1.Min == lengthValidator2.Min && lengthValidator1.Max == lengthValidator2.Max;
+
+                if (x is IComparisonValidator comparisonValidator1 &&
+                    y is IComparisonValidator comparisonValidator2)
+                {
+                    return comparisonValidator1.Comparison == comparisonValidator2.Comparison &&
+                           Equals(comparisonValidator1.MemberToCompare, comparisonValidator2.MemberToCompare) &&
+                           Equals(comparisonValidator1.ValueToCompare, comparisonValidator2.ValueToCompare);
+                }
+
+                if (x is IBetweenValidator betweenValidator1 &&
+                    y is IBetweenValidator betweenValidator2)
+                    return Equals(betweenValidator1.From, betweenValidator2.From) && Equals(betweenValidator1.To, betweenValidator2.To);
+
+                return false;
+            }
+
+            /// <inheritdoc />
+            public int GetHashCode(IPropertyValidator? obj)
+            {
+                return obj?.GetHashCode() ?? 0;
+            }
         }
     }
 }
---
+++ test/MicroElements.Swashbuckle.FluentValidation.Tests/RuleHistoryCacheTest.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using MicroElements.OpenApi.FluentValidation;
+using System.Linq;
+using Xunit;
+
+namespace MicroElements.Swashbuckle.FluentValidation.Tests
+{
+    public class RuleHistoryCacheTest
+    {
+        public class Sample
+        {
+            public string? Name { get; set;}
+        }
+
+        public class SampleValidator : AbstractValidator<Sample>
+        {
+            public SampleValidator()
+            {
+                RuleFor(p => p.Name)
+                    .Matches("^[a-zA-Z0-9 ]+$");
+            }
+        }
+
+        [Fact]
+        // Issue 143 https://github.com/micro-elements/MicroElements.Swashbuckle.FluentValidation/issues/143
+        public void DuplicateCachItem()
+        {
+            var key = new object();
+            var cacheItem1 = CreateCacheItem();
+            var cacheItem2 = CreateCacheItem();
+
+            key.AddRuleHistoryItem(cacheItem1);
+
+            Assert.True(key.ContainsRuleHistoryItem(cacheItem2));
+        }
+
+        private RuleHistoryCache.RuleCacheItem CreateCacheItem()
+        {
+            var sampleValidator = new SampleValidator();
+            var propertyValidator = sampleValidator.CreateDescriptor().GetRulesForMember("Name").First().Components.First().Validator;
+            return new RuleHistoryCache.RuleCacheItem("MySchema", "MyProperty", propertyValidator, "Pattern");
+        }
+    }
+}
